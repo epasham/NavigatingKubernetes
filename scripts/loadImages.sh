e9e82c36e4e81dae68b7cd1ba620344fb528c34b
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-12-09 16:54:09 +0530

    Create loadImages.sh

---
+++ scripts/loadImages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Directory containing tar files
+IMAGE_DIR="$HOME/ica-app/docker-images"
+
+# Loop through all tar files
+for tarfile in "$IMAGE_DIR"/*.tar; do
+    echo "Found tar file: $tarfile"
+
+    # Prompt user for confirmation
+    read -p "Do you want to load this image? (y/n): " choice
+
+    if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+        echo "Loading image from $tarfile..."
+        IMAGE_NAME=$(docker load -i "$tarfile" )
+
+        echo "Image $IMAGE_NAME loaded successfully."
+    else
+        echo "Skipping $tarfile"
+    fi
+done
+
+echo "All images processed."
