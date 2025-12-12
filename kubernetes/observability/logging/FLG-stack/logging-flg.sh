425fa397b3eb6d883ef70efadc5f82a9a88d2442
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-12-12 23:43:23 +0530

    Update logging-flg.sh

--- kubernetes/observability/logging/FLG-stack/logging-flg.sh
+++ kubernetes/observability/logging/FLG-stack/logging-flg.sh
@@ -14,16 +14,16 @@ else
 fi
 
 print_header "installing Loki"
-helm upgrade --install  loki grafana/loki -n observability --create-namespace -f ../ALG-stack/loki-values.yml
+helm upgrade --install  loki grafana/loki -n observability --create-namespace -f ../ALG-stack/loki-values.yaml
 
 
 print_header "installing Grafana dashboard"
-helm upgrade --install grafana grafana/grafana -n observability --create-namespace -f ../ALG-stack/grafana-values.yml --wait
+helm upgrade --install grafana grafana/grafana -n observability --create-namespace -f ../ALG-stack/grafana-values.yaml --wait
 
 
 print_header "install Fluent-bit"
 helm repo add fluent https://fluent.github.io/helm-charts && helm repo update
 helm upgrade --install fluent-bit fluent/fluent-bit -n observability --create-namespace -f fluent-bit-values.yaml --version 0.50.0
 
-print_header "admin password: $(kubectl get secret --namespace grafana grafana-dashboard -o jsonpath="{.data.admin-password}" | base64 --decode)"
-print_header "kubectl -n observability port-forward --address 0.0.0.0 svc/grafana 8080:80 &"
+print_header "admin password: $(kubectl -n observability get secret grafana -o jsonpath="{.data.admin-password}" | base64 --decode)"
+print_header "run this command to access grafana dashboard: kubectl -n observability port-forward --address 0.0.0.0 svc/grafana 8080:80 &"
