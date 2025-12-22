9366468b1eb374d785dd309d8c0a1a8d23bc09a2
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-12-22 16:25:16 +0530

    Update init.sh

--- scripts/common/init.sh
+++ scripts/common/init.sh
@@ -9,3 +9,12 @@ function print_header() {
   echo "---------------------------------------------------------------------"
   echo ""
 }
+
+https://piotrminkowski.com/2021/11/11/kafka-streams-with-spring-cloud-stream/
+https://piotrminkowski.com/2022/01/24/distributed-transactions-in-microservices-with-kafka-streams-and-spring-boot/
+https://github.com/piomin/sample-spring-boot-on-kubernetes
+https://github.com/piomin/sample-spring-microservices-new
+https://github.com/piomin/course-kubernetes-microservices
+https://github.com/piomin/sample-spring-cloud-security
+
+https://github.com/collabnix/kubetools
